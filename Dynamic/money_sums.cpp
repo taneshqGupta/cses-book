975a3403fca2304023c57df3fbc2d8acb8179e4a
taneshqGupta <f.taneshq@gmail.com>
2026-07-27 08:31:10 +0530

    update

---
+++ Dynamic/money_sums.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define nl '\n'
+#define all(x) (x).begin(), (x).end()
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll n;
+    cin >> n;
+    vector<ll> coins(n);
+    for (ll &c : coins) cin >> c;
+
+    vector<bool> dp(1e5 + 5, false);
+    dp[0] = true;
+
+    for (ll c : coins) {
+        for (ll i = 1e5; i >= c; --i) {
+            if (dp[i - c]) {
+                dp[i] = true;
+            }
+        }
+    }
+
+    ll count = 0;
+    for (ll i = 1; i <= 1e5; ++i) {
+        if (dp[i]) count++;
+    }
+    cout << count << nl;
+    for (ll i = 1; i <= 1e5; ++i) {
+        if (dp[i]) cout << i << ' ';
+    }
+    cout << nl;
+}
